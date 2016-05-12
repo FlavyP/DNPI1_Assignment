c80b1efdaeee8d0f365d19467745ecf8cff8a993
Nikola Peevski <jeratull@hotmail.com>
2016-05-12 10:14:22 +0200

    First commit

---
+++ CottageWars/CottageWars/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CottageWars.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CottageWars/CottageWars/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CottageWars.Startup))]
+namespace CottageWars
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
