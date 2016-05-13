a63ae67b01513c3b2ca307292ff464ee4ffaf2bd
Nikola Peevski <jeratull@hotmail.com>
2016-05-13 21:49:29 +0200

    Bootstrap finally working yay and it has frozen width .

--- CottageWars/CottageWars/MasterPage.Master.designer.cs
+++ CottageWars/CottageWars/MasterPage.Master.designer.cs
@@ -13,30 +13,12 @@ namespace CottageWars {
     public partial class MasterPage {
         
         /// <summary>
-        /// title control.
+        /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder title;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
